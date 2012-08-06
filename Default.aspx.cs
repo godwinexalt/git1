f81a7b4e74f0104fc0e02f5bdb4ee2fa197b604f
godwinexalt <godwin.samson@eisplc.com>
2012-08-06 17:02:49 +0530

    file create

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
-
+        if (!File.Exists(Server.MapPath("~/text.txt")))
+            File.Create(Server.MapPath("~/text.txt"));
 
     }
 }
